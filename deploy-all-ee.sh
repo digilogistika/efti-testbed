583608303f9925d3e2ed10cfae1e2022fa68363f
Anton Keks <anton@azib.net>
2026-01-28 13:59:58 +0200

    add deploy script

---
+++ deploy-all-ee.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+cd ../efti-gate-poc || echo "Clone the efti-gate-poc repository first" && exit 1
+
+gates=("eu-ee31" "eu-ee32")
+
+for gate in "${gates[@]}"; do
+  echo "***** Deploying $gate ******"
+  ./deploy.sh "$gate"
+done
+echo "***** All gates deployed ******"
